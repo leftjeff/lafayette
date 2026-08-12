89d691d103c661695a784a71380139afdc29e779
Jeff Cox <jeffcox@gmail.com>
2026-08-12 13:26:13 +0000

    Redirect lafayettepointerpark.com to .org

--- vercel.ts
+++ vercel.ts
@@ -1,9 +1,23 @@
-import { type VercelConfig } from "@vercel/config/v1";
+import { matchers, type VercelConfig } from "@vercel/config/v1";
 
 export const config: VercelConfig = {
-  git: {
-    deploymentEnabled: {
-      main: false,
-    },
-  },
+	git: {
+		deploymentEnabled: {
+			main: false,
+		},
+	},
+	redirects: [
+		{
+			source: "/:path(.*)",
+			has: [matchers.host("lafayettepointerpark.com")],
+			destination: "https://lafayettepointerpark.org/:path",
+			permanent: true,
+		},
+		{
+			source: "/:path(.*)",
+			has: [matchers.host("www.lafayettepointerpark.com")],
+			destination: "https://lafayettepointerpark.org/:path",
+			permanent: true,
+		},
+	],
 };
